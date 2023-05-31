cc458ac2dd5fad1da2a8c5d7078a94a22ae278e2
neyneyalldayday <drfrankenstein123@gmail.com>
2023-05-31 14:20:09 -0500

    messin

--- script.js
+++ script.js
@@ -9,7 +9,12 @@ var dummy = document.querySelector(".dummy");
 var inputForm = document.querySelector("#inputForm");
 var timeLeftDisplay = document.querySelector("#time-left");
 var startBtn = document.querySelector("#start-button");
-var endForm = document.querySelector(".end-form")
+var endForm = document.querySelector(".end-form");
+var scoreSection = document.querySelector(".score-section");
+var startScreen = document.querySelector(".start-text");
+var revealNames = document.querySelector("#high-scores");
+var endButtonEl = document.querySelector('.end-button-div')
+
 timeLeft = 75;
 let flash;
 let flashend;
@@ -44,22 +49,22 @@ var quiz = [
     correctAnswer: "===",
     userAnswer: null,
   },
-  
 ];
 
-
 function countDown() {
-    startBtn.classList.add("hide");
-    timer = setInterval(function () {
-      if (timeLeft <= 0) {
-        endQuiz();
-      }
-      timeLeftDisplay.innerHTML = timeLeft;
-      timeLeft -= 1;
-    }, 1000);
-  }
+  displayQuestion(quiz[currentIndex]);
+  startBtn.classList.add("hide");
+  timer = setInterval(function () {
+    if (timeLeft <= 0) {
+      endQuiz();
+    }
+    timeLeftDisplay.innerHTML = timeLeft;
+    timeLeft -= 1;
+  }, 1000);
+}
 
 function displayQuestion(question) {
+  startScreen.classList.add("hide");
   var HTML = `            
                 <P>${question.questionText}</P>
                 <button class ="answerButton">${question.answers[0]}</button>
@@ -71,23 +76,21 @@ function displayQuestion(question) {
   questionDiv.innerHTML = HTML;
 }
 
-displayQuestion(quiz[currentIndex]);
-
 document.addEventListener("click", function (event) {
   if (event.target.classList.contains("answerButton")) {
     quiz[currentIndex].userAnswer = event.target.textContent;
     if (quiz[currentIndex].userAnswer == quiz[currentIndex].correctAnswer) {
-        clearTimeout(flash);       
-        dummy.textContent = "Correct Foo!!"; 
-        dummy.classList.remove("hide")       
-        flash = setTimeout(function () {
+      clearTimeout(flash);
+      dummy.textContent = "Correct Foo!!";
+      dummy.classList.remove("hide");
+      flash = setTimeout(function () {
         dummy.classList.add("hide");
-      }, 1000);     
+      }, 1000);
       score++;
     } else {
-        clearTimeout(flashDummy);        
+      clearTimeout(flash);
       dummy.textContent = "Wrong Foo!!!!";
-        dummy.classList.remove("hide")
+      dummy.classList.remove("hide");
       flash = setTimeout(function () {
         dummy.classList.add("hide");
       }, 1000);
@@ -95,23 +98,21 @@ document.addEventListener("click", function (event) {
     }
     nextQuestion();
   }
-
 });
 
 function nextQuestion() {
   currentIndex++;
-  if (currentIndex  === quiz.length) {
-    clearInterval(timer)
+  if (currentIndex === quiz.length) {
+    clearInterval(timer);
     endQuiz();
   } else {
-    displayQuestion(quiz[currentIndex])
+    displayQuestion(quiz[currentIndex]);
   }
 }
 
 function endQuiz() {
   const endScreen = document.querySelector(".end-screen");
   endScreen.classList.remove("hide");
-  
 }
 
 function saveShit() {
@@ -123,21 +124,61 @@ function saveShit() {
   };
   localShit.push(scores);
   localStorage.setItem("localShit", JSON.stringify(localShit));
- inputForm.value = ""
- var message = document.createElement("p")
- message.textContent = "name has been saved"
- endForm.appendChild(message)
- let flashend = setTimeout(function(){
-    message.classList.add("hide")
- }, 1000)
- clearTimeout(flashend)
+  inputForm.value = "";
+  var message = document.createElement("p");
+  message.textContent = "name has been saved";
+  endForm.appendChild(message);
+  let flashend = setTimeout(function () {
+    message.classList.add("hide");
+  }, 1000);
+  clearTimeout(flashend);
+  resetQuiz()
 }
 
+function displayScores() {
 
-  startBtn.addEventListener("click", countDown);
+  let userScores = JSON.parse(localStorage.getItem("localShit")) || [];
+
+  for (let i = 0; i < userScores.length; i++) {
+    let scoreScreen = document.createElement("li");
+    scoreScreen.classList.add("score-item");
+    scoreScreen.textContent =
+      userScores[i].doodsName + ":" + userScores[i].score;
+    scoreSection.append(scoreScreen);
+    scoreSection.classList.remove('hide')
+  }
+
+  var closeScores = document.createElement('button')
+  closeScores.innerHTML =`X`;
+  closeScores.classList.add('close-scores-button')
+  scoreSection.append(closeScores)
+  document.addEventListener("click", function(event){
+    if(event.target.classList.contains("close-scores-button")){
+      scoreSection.classList.add('hide')
+    }
+  })
+ 
+
+}
+
+function resetQuiz(){
+  var endButton = `
+  <button class ="endButton">Click me to restart the quiz</button>
+  `;
+  endButtonEl.innerHTML = endButton;
+document.addEventListener("click", function(event) {
+  if(event.target.classList.contains('endButton')){
+    location.reload()
+  }
+})
+}
+
+startBtn.addEventListener("click", countDown);
 
 const submitName = document.querySelector("#submit-button");
 submitName.addEventListener("click", function (event) {
   event.preventDefault();
   saveShit();
 });
+
+revealNames.addEventListener("click", displayScores);
